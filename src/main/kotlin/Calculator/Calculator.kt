7b894291863ceb68cdd1688d2ad3a43a2cbecc2a
Moxsy2626 <david.gonzalez.moxsy@est.una.ac.cr>
2025-08-05 20:58:57 -0600

    Commit 1: Se crea la clase Calculator y se ingresa en la carpeta Calculator con funciones básicas

---
+++ src/main/kotlin/Calculator/Calculator.kt
@@ -0,0 +1,17 @@
+package calculator
+
+class Calculator {
+
+    fun add(a: Double, b: Double): Double = a + b
+
+    fun subtract(a: Double, b: Double): Double = a - b
+
+    fun multiply(a: Double, b: Double): Double = a * b
+
+    fun divide(a: Double, b: Double): Double {
+        if (b == 0.0) {
+            throw IllegalArgumentException("Cannot divide by zero")
+        }
+        return a / b
+    }
+}
